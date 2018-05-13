d9db7cb0805cec82a1f0dfa48dd8389b66eb8a48
Ilham Wahabi <wahabiputra@gmail.com>
2018-05-14 06:42:56 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from "./pages/Home";
+
+export default {
+  name: "app",
+  components: {
+    Home
+  }
+};
+</script>
+
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;  
+  }
+
+  #app {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    font-family: 'Quicksand', sans-serif;
+    min-height: 100vh;
+    font-size: 10px;
+  }
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <main id="main">
+    <h1 class="main__title">Welcome to Anime Genesis</h1>
+    <div class="main__option">
+      <div class="main__desc main__desc-voice">
+        <ion-icon name="create"></ion-icon>
+      </div>
+      <div class="main__desc main__desc-text">
+        <ion-icon name="text"></ion-icon>
+      </div>
+    </div>
+    <h2 class="main__subtitle">Please choose your describing method</h2>
+  </main>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="scss" scoped>
+  #main {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+  }
+
+  .main__title {
+    font-size: 3rem;
+  }
+
+  .main__option {
+    display: flex;
+    flex-direction: row;
+    font-size: 5rem;
+  }
+
+  .main__desc {
+    margin: 2.5rem;
+    border-radius: 50%;
+    width: 10rem;
+    height: 10rem;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .main__desc-voice {
+    background-color: #F44336;
+  }
+
+  .main__desc-text {
+    background-color: #2196F3;    
+  }
+
+
+</style>
